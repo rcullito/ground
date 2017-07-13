b6f29514ae9cc0df07c62d59d5ed49b39a71b7d0
Rob Culliton <rculliton@quartethealth.com>
2017-07-12 22:17:23 -0400

    It's a process

--- src/n/core.clj
+++ src/n/core.clj
@@ -1,6 +1,24 @@
 (ns n.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def person
+  {:name "Nancy"
+   :dog  "Claire"
+   :age 37})
+
+
+
+(defmacro n
+  "takes a number of formes and magically transforms them"
+  [& forms]
+  `(do ~@forms))
+
+(n (println "sure")
+   (println "then came the war old sport!"))
+
+
+;;; ideally our syntax should read
+(when-let [val (n
+                (n2 (:age person))
+                (n? (> 0)))]
+  ("it's working!!!"))
+
