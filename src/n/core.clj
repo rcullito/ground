30221487f255b43584cc2fea4aa39beb0ade0021
Rob Culliton <rculliton@quartethealth.com>
2017-07-14 09:03:13 -0400

    A snapshot of the working era

--- src/n/core.clj
+++ src/n/core.clj
@@ -5,28 +5,51 @@
    :dog  "Claire"
    :age 37})
 
-(defmacro n
-  "takes a number of formes and magically transforms them"
-  [expr & forms]
-  (let [g (gensym)]
-    `(let [~g ~expr
-           ~@(interleave (repeat g) (butlast forms))]
-        ~(if (empty? forms)
-          g
-          (last forms)))))
+;; basically interleave with the ability to skp a step
 
-;; the charter of this macro is to provide the best of threading
-;; some, and cond
+(defn n?
+  [form]
+  (if form
+    (second form)
+    ;; by this we really mean the whole thing shoudl return nil, like some->
+    nil))
+
+(defn n?
+  [number]
+  (> number 0))
+
+(defmacro n
+  [x & forms]
+  (loop [x x, forms forms]
+    (if forms
+      (let [form (first forms)
+            threaded (if (seq? form)
+                       `(~(first form) ~x ~@(next form))
+                       (list form x))]
+        (recur threaded (next forms)))
+      x)))
+
+
+;; something like
+;; eventually this part needs to return either nil or the thing that was passed in if it was true
+
+(if (= (form 'n1))
+  (do (println "it's working")
+      ))
 
 
 ;;; ideally our syntax should read
 (n person
    (:age)
-   (> 0))
+   n?)
 
 
+#_
 (-> person
     :age
     (> 0))
-
-
+#_
+(n person
+   (:age)
+   n?
+   inc)
