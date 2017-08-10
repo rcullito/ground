16315e8d8b29b282debdb0403b9486b8f669ba7e
Rob Culliton <rculliton@quartethealth.com>
2017-08-10 17:11:47 -0400

    Guard against single form. (Not in seq). Wrap n expression in the same nil check that all other forms get

--- src/n/core.clj
+++ src/n/core.clj
@@ -1,16 +1,23 @@
 (ns n.core)
 
+(defn handle-n-form
+  [g step]
+  (let [post-n-step (second step)]
+    `(if (nil? ~g) nil
+         (if (~(first post-n-step) ~g ~@(next post-n-step))
+           ~g
+           nil))))
+
 (defmacro n
   "like some->, except forms following n are treated as predicates
 n  that do not affect the value passed to the next form."
   [expr & forms]
   (let [g (gensym)
         steps (map (fn [step]
-                     (if (= (first step) 'n)
-                       (let [post-n-step (second step)]
-                         `(if (~(first post-n-step) ~g ~@(next post-n-step))
-                            ~g
-                            nil))
+                     (if (seq? step)
+                       (if (= (first step) 'n)
+                       (handle-n-form g step)
+                       `(if (nil? ~g) nil (-> ~g ~step)))
                        `(if (nil? ~g) nil (-> ~g ~step))))
                    forms)]
     `(let [~g ~expr
