78ec0c6c73c8ad6185fcc9a6d075a4895724f617
Rob Culliton <rculliton@quartethealth.com>
2017-08-20 08:28:25 -0400

    Add support for some->> based on sequential check

--- src/n/core.clj
+++ src/n/core.clj
@@ -1,10 +1,10 @@
 (ns n.core)
 
 (defn handle-n-form
-  [g step]
+  [threader g step]
   (let [post-n-step (second step)]
     `(if (nil? ~g) nil
-         (if (-> ~g ~step)
+         (if (~threader ~g ~post-n-step)
            ~g
            nil))))
 
@@ -13,10 +13,13 @@
   that do not affect the value passed to the next form."
   [expr & forms]
   (let [g (gensym)
+        threader (if (sequential? expr)
+                   '->>
+                   '->)
         steps (map (fn [step]
                      (if ((every-pred seq? #(= (first %) 'n)) step)
-                       (handle-n-form g step)
-                       `(if (nil? ~g) nil (-> ~g ~step))))
+                       (handle-n-form threader g step)
+                       `(if (nil? ~g) nil (~threader ~g ~step))))
                    forms)]
     `(let [~g ~expr
            ~@(interleave (repeat g) (butlast steps))]
