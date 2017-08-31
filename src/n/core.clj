0d30ba09828dba0fba10872ef7f6ade0c7eb1c65
Rob Culliton <rob.culliton@gmail.com>
2017-08-31 09:02:31 -0400

    split out step-fn

--- src/n/core.clj
+++ src/n/core.clj
@@ -11,23 +11,25 @@
            ~g
            nil))))
 
+(defn step-fn
+  [g step]
+  (if ((every-pred seq? #(= (first %) 'n)) step)
+    (handle-n-form g step)
+    `(if (nil? ~g)
+       nil
+       (if (sequential? ~g)
+         (->> ~g ~step)
+         (-> ~g ~step)))))
+
 (defmacro n
   "like some->, except forms following n are treated as predicates
   that do not affect the value passed to the next form."
   [expr & forms]
   (let [g (gensym)
-        steps (map (fn [step]
-                     (if ((every-pred seq? #(= (first %) 'n)) step)
-                       (handle-n-form g step)
-                       `(if (nil? ~g) nil (if (sequential? ~g)
-                                            (->> ~g ~step)
-                                            (-> ~g ~step)))))
-                   forms)]
+        steps (map (partial step-fn g) forms)]
     `(let [~g ~expr
            ~@(interleave (repeat g) (butlast steps))]
        ~(if (empty? steps)
           g
           (last steps)))))
-
-
 
