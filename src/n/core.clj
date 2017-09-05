4fe60ff6b6002e7c50b82bcb7e2d6382cf14fa90
Rob Culliton <rculliton@quartethealth.com>
2017-09-05 17:34:24 -0400

    Update with n->>

--- src/n/core.clj
+++ src/n/core.clj
@@ -11,3 +11,15 @@
               `(some-> ~acc ~x)))
           expr
           forms))
+
+(defmacro n->>
+  "like some->>, except predicates prefixed with n either pass through the prior result or return nil for the entire form."
+  [expr & forms]
+  (reduce (fn [acc x]
+            (if (= 'n (first x))
+              `(if (some->> ~acc ~(second x))
+                 ~acc
+                 nil)
+              `(some->> ~acc ~x)))
+          expr
+          forms))
--- test/n/core_test.clj
+++ test/n/core_test.clj
@@ -1,6 +1,6 @@
 (ns n.core-test
   (:require [clojure.test :refer [deftest testing is]]
-            [n.core :refer [n->]]))
+            [n.core :refer [n-> n->>]]))
 
 (def person
   {:name "Nancy"
@@ -22,3 +22,14 @@
                   (n (> 40))
                   (+ 10))))))
 
+(deftest truthy-predicate-returns-collection
+  (testing "that the predicate passes through prior value if true"
+    (is (= '(2 3 4) (n->> [1 2 3]
+                     (n (every? identity))
+                     (map inc))))))
+
+(deftest predicate-collection-returns-nil
+  (testing "that a false predicate returns nil for the entire form"
+    (is (= nil (n->> [1 2 nil 3]
+                     (n (every? identity))
+                     (map inc))))))
