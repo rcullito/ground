6dd06b21c1ff78c895f247ac1d8e27750615fba1
Rob Culliton <rculliton@quartethealth.com>
2017-09-11 11:49:50 -0400

    Reuse code across n fns

--- src/ground/core.clj
+++ src/ground/core.clj
@@ -1,19 +1,23 @@
 (ns ground.core)
 
+(defn n-thread
+  [expr forms operator]
+  (reduce (fn [acc x]
+            (if (= 'n (first x))
+              `(if (~operator ~acc ~(second x))
+                 ~acc
+                 nil)
+              `(~operator ~acc ~x)))
+          expr
+          forms))
+
 (defmacro n->
   "within -> threading, include predicates after the symbol `n` to either forward 
   the prior expression if true, or return nil for the entire form if false.
   Assuming the prior expression resulted in an integer, a valid n predicate might be:
   `(n (> 3))`"
   [expr & forms]
-  (reduce (fn [acc x]
-            (if (= 'n (first x))
-              `(if (some-> ~acc ~(second x))
-                 ~acc
-                 nil)
-              `(some-> ~acc ~x)))
-          expr
-          forms))
+  (n-thread expr forms 'some->))
 
 (defmacro n->>
   "within ->> threading, include predicates after the symbol `n` to either forward 
@@ -21,14 +25,7 @@
   Assuming the prior expression resulted in a collection, a valid n predicate might be:
   `(n (every? identity))`"
   [expr & forms]
-  (reduce (fn [acc x]
-            (if (= 'n (first x))
-              `(if (some->> ~acc ~(second x))
-                 ~acc
-                 nil)
-              `(some->> ~acc ~x)))
-          expr
-          forms))
+  (n-thread expr forms 'some->>))
 
 (defn- try-catch-thread
   [expr forms operator]
