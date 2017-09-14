9a2bde0dc96c4f1fc41437314e6d94b3ebdcd0d2
Rob Culliton <rculliton@quartethealth.com>
2017-09-14 14:51:50 -0400

    Clean up codebase around multiple n forms (base, ?, and !)
    Todo: write tests for ? and !

--- src/ground/core.clj
+++ src/ground/core.clj
@@ -1,15 +1,32 @@
 (ns ground.core)
 
+
+(defn- intended-predicate
+  "determine if the first symbol of the form is intended to signal that the subsequent
+  form should be treated as a predicate"
+  [form]
+  (and
+   (seq? form)
+   ;; for backwards compatibility treat either n or n? as indicative of a predicate
+   (let [signal (first form)]
+     (or (= 'n signal)
+        (= 'n? signal)))))
+
+(defn- intended-side-effect
+  "determine if the first symbol of the form is intended to signal that the subsequent
+  form should only produce side effects"
+  [form]
+  (and (seq? form) (= 'n! (first form))))
+
 (defn- n-thread
   [expr forms operator]
   (reduce (fn [acc x]
-            ;; for backwards compatibility treat either n or n? as indicative of a predicate
-            (cond (and (seq? x) (or (= 'n (first x)) (= 'n? (first x)))) `(if (~operator ~acc ~(second x))
-                                                                            ~acc
-                                                                            nil)
-                  (and (seq? x) (= 'n! (first x)))                       `(do (~operator ~acc ~(second x))
-                                                                              ~acc)
-                  :else                                                  `(~operator ~acc ~x)))
+            (cond (intended-predicate x)   `(if (~operator ~acc ~(second x))
+                                              ~acc
+                                              nil)
+                  (intended-side-effect x) `(do (~operator ~acc ~(second x))
+                                                ~acc)
+                  :else                    `(~operator ~acc ~x)))
           expr
           forms))
 
